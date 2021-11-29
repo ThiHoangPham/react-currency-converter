fee2f653b1f060ab893934b63f22bef57a59a665
Adam Pham <thaihoangpham2008@gmail.com>
2021-11-29 08:23:12 +0800

    set API fetch function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import CurrencyRow from './CurrencyRow';
 
+const BASE_URL = "http://data.fixer.io/api/latest?access_key=be1118ee9373259facf451d84f94e4ea"
+
 function App() {
+
+  const =
+
+    useEffect(() => {
+      fetch(BASE_URL)
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }, [])
+
   return (
     <>
       <h1>Convert</h1>
